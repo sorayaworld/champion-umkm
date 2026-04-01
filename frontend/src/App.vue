6d6e71a74a9cfeb412ee259aab395dc6f9d41fca
fafago1m <alfacastel3@gmail.com>
2026-04-01 13:31:33 +0700

    feat:setup vue js, axioss,  sweetalert2

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="flex items-center justify-center p-6">
+    <div class="bg-white p-8 rounded-2xl">
+      <h1 class="text-3xl font-extrabold text-blue-600">
+        test tailwinddddd
+      </h1>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
